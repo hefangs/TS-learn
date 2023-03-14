482a7a7a98d03c1a037367e76a9de73254c2efa1
何方 <he529564582@163.com>
2023-03-14 22:15:46 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let str = 'hi';
+let num1 = 123;
+let num2 = NaN;
+let num3 = Infinity;
+let n = null;
+let u = undefined;
+// let v1: void = null
+let v2 = undefined;
+console.log(str);
+console.log(num1, num2, num3);
+console.log(n, u);
+console.log(v2);
---
+++ index.ts
@@ -0,0 +1,13 @@
+let str: string = 'hi'
+let num1: number = 123
+let num2: number = NaN
+let num3: number = Infinity
+let n: null = null
+let u: undefined = undefined
+// let v1: void = null
+let v2: void = undefined
+
+console.log(str)
+console.log(num1, num2, num3)
+console.log(n, u)
+console.log(v2)
