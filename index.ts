e3a8c789fe0eef0cae6769eae9891b7750000f99
何方 <he529564582@163.com>
2023-03-18 23:08:56 +0800

    update:函数

--- index.ts
+++ index.ts
@@ -368,12 +368,12 @@ class Button extends Control implements SelectableControl {
 class TextBox extends Control {
   select() {}
 }
-class ImageControl implements SelectableControl {
-  // Error: Class 'ImageControl' incorrectly implements interface 'SelectableControl'.
-  //  Types have separate declarations of a private property 'state'.
-  private state: any
-  select(): void {}
-}
+// class ImageControl implements SelectableControl {
+//   // Error: Class 'ImageControl' incorrectly implements interface 'SelectableControl'.
+//   //  Types have separate declarations of a private property 'state'.
+//   private state: any
+//   select(): void {}
+// }
 // 在上面的例子里， SelectableControl 包含了 Control 的所有成员，包括私有成员 state 。
 // 因为 state 是私有成员，所以只能够是 Control 的子类们才能实现 SelectableControl 接口。
 // 因为只有 Control 的子类才能够拥有一个声明于 Control 的私有成员 state ，这对私有成员的兼容性是必需的。
@@ -382,3 +382,143 @@ class ImageControl implements SelectableControl {
 // 实际上， SelectableControl 就像 Control 一样，并拥有一个 select 方法。
 //  Button 和 TextBox 类是 SelectableControl 的子类（因为它们都继承自 Control 并有 select 方法）。
 //  而对于 ImageControl 类，它有自身的私有成员 state 而不是通过继承 Control 得来的，所以它不可以实现 SelectableControl 。
+
+//函数类型
+
+function add(x: number, y: number): number {
+  return x + y
+}
+
+function fullName(firstName: string, lastName?: string): string {
+  if (lastName) {
+    return firstName + lastName
+  } else {
+    return firstName
+  }
+}
+let result1 = fullName('Bob')
+let result2 = fullName('Bob', 'Adams')
+console.log(result1)
+console.log(result2)
+// 把 last name 的默认值设置为 "Smith"
+
+function buildName(firstName: string, lastName = 'Smith') {
+  return firstName + lastName
+}
+let res1 = buildName('Bob')
+let res2 = buildName('Bob', undefined)
+let res3 = buildName('Bob', 'Adams')
+
+console.log('####', res1)
+console.log(res2)
+console.log(res3)
+
+function buildName1(firstName = 'Will', lastName: string) {
+  return firstName + lastName
+}
+// let re1 = buildName1('Bob') // error, too few parameters
+// let re2 = buildName1('Bob', 'Adams', 'Sr.') // error, too many parameters
+let re3 = buildName1('Bob', 'Adams') // okay and returns "Bob Adams"
+let re4 = buildName1(undefined, 'Adams')
+console.log(re3, re4)
+
+// 剩余参数
+function personInfo(name: string, ...otherProps: string[]) {
+  return name + otherProps.join('')
+}
+let employee = personInfo('张三', '男', '20')
+console.log(employee)
+
+// let deck = {
+//   suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+//   cards: Array(52),
+//   createCardPicker: function () {
+//     return () => {
+//       let pickedCard = Math.floor(Math.random() * 52)
+//       let pickedSuit = Math.floor(pickedCard / 13)
+
+//       return { suit: this.suits[pickedSuit], card: pickedCard % 13 }
+//     }
+//   }
+// }
+
+// let cardPicker = deck.createCardPicker()
+// let pickedCard = cardPicker()
+
+// this 参数
+function f1(this: void) {}
+
+interface Card {
+  suit: string
+  card: number
+}
+interface Deck {
+  suits: string[]
+  cards: number[]
+  createCardPicker(this: Deck): () => Card
+}
+let deck: Deck = {
+  suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+  cards: Array(52),
+  // NOTE: The function now explicitly specifies that its callee must be of type Deck
+  createCardPicker: function (this: Deck) {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52)
+      let pickedSuit = Math.floor(pickedCard / 13)
+
+      return { suit: this.suits[pickedSuit], card: pickedCard % 13 }
+    }
+  }
+}
+let cardPicker = deck.createCardPicker()
+let pickedCard = cardPicker()
+console.log('card: ' + pickedCard.card + ' of ' + pickedCard.suit)
+
+interface UIElement {
+  addClickListener(onclick: (this: void, e: Event) => void): void
+}
+
+// 重载
+// let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+// function pickCard(x: any): any {
+//   // Check to see if we're working with an object/array
+//   // if so, they gave us the deck and we'll pick the card
+//   if (typeof x == 'object') {
+//     let pickedCard = Math.floor(Math.random() * x.length)
+//     return pickedCard
+//   }
+//   // Otherwise just let them pick the card
+//   else if (typeof x == 'number') {
+//     let pickedSuit = Math.floor(x / 13)
+//     return { suit: suits[pickedSuit], card: x % 13 }
+//   }
+// }
+
+let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+
+function pickCard(x: { suit: string; card: number }[]): number
+function pickCard(x: number): { suit: string; card: number }
+function pickCard(x: any): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == 'object') {
+    let pickedCard = Math.floor(Math.random() * x.length)
+    return pickedCard
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == 'number') {
+    let pickedSuit = Math.floor(x / 13)
+    return { suit: suits[pickedSuit], card: x % 13 }
+  }
+}
+
+let myDeck = [
+  { suit: 'diamonds', card: 2 },
+  { suit: 'spades', card: 10 },
+  { suit: 'hearts', card: 4 }
+]
+let pickedCard1 = myDeck[pickCard(myDeck)]
+alert('card: ' + pickedCard1.card + ' of ' + pickedCard1.suit)
+
+let pickedCard2 = pickCard(15)
+alert('card: ' + pickedCard2.card + ' of ' + pickedCard2.suit)
