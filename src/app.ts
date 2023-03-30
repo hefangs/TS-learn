2d7845c7a89281650ff1a36e074b0e2fe0c3f11f
何方 <he529564582@163.com>
2023-03-31 00:23:43 +0800

    mixins

--- src/app.ts
+++ src/app.ts
@@ -1920,7 +1920,7 @@
 // console.log(Object.keys(obj)) // [ 'name' ]
 // // Object.getOwnPropertyNames 也读不到symbol
 // console.log(Object.getOwnPropertyNames(obj)) // [ 'name' ]
-// // Object.setOwnPropertyNames  读到symbol但是name读不到
+// // Object.getOwnPropertySymbols  读到symbol但是name读不到
 // console.log(Object.getOwnPropertySymbols(obj)) // [ Symbol(1), Symbol(2) ]
 // // Reflect.ownKeys可以全部读到
 // console.log(Reflect.ownKeys(obj)) // [ 'name', Symbol(1), Symbol(2) ]
@@ -2039,3 +2039,61 @@
 
 // /// <reference path="./app1.ts" />
 // /// <reference path="./app2.ts" />
+
+// declare
+// 当使用第三方库时，我们需要引用它的声明文件，
+// 才能获取对应的代码补全和接口调试功能
+// import axios from 'axios'
+// import express from 'express'
+
+// mixins
+// 对象混入
+interface Name {
+  name: string
+}
+interface Age {
+  age: number
+}
+interface Sex {
+  sex: string
+}
+let a: Name = { name: 'John' }
+let b: Age = { age: 18 }
+let c: Sex = { sex: 'female' }
+let obj = Object.assign(a, b, c)
+// console.log(obj)
+
+// class
+class A {
+  name: string
+  showName(): string {
+    return this.name
+  }
+}
+class B {
+  age: number
+  showAge(): number {
+    return this.age
+  }
+}
+class C implements A, B {
+  name: string = 'Mark'
+  age: number = 18
+  showName: () => string
+  showAge: () => number
+}
+mixins(C, [A, B])
+function mixins(curCls: any, itemCls: any[]) {
+  itemCls.forEach(item => {
+    // console.log(Object.getOwnPropertyNames(item.prototype)) // [ 'constructor', 'showName' ][ 'constructor', 'showAge' ]
+    Object.getOwnPropertyNames(item.prototype).forEach(name => {
+      console.log(item.prototype[name]) // [class A] [Function: showName] [class B][Function: showAge]
+      curCls.prototype[name] = item.prototype[name]
+    })
+  })
+}
+let ccc = new C()
+console.log(ccc.name)
+console.log(ccc.age)
+console.log(ccc.showName())
+console.log(ccc.showAge())
